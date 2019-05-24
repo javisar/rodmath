965b428d6031dbb8507923d89c1c5c7de8408981
Rodol Phito <rodolvertice@gmail.com>
2019-05-24 01:34:21 -0700

    uint bit ops | & ^

--- uint2.cs
+++ uint2.cs
@@ -31,6 +31,18 @@ namespace RodMath
 		public static uint2 operator /(uint2 a, uint2 b) => new uint2(a.x / b.x, a.y / b.y);
 		public static uint2 operator /(uint2 a, uint  b) => new uint2(a.x / b  , a.y / b  );
 		public static uint2 operator /(uint  a, uint2 b) => new uint2(a   / b.x, a   / b.y);
+
+		public static uint2 operator &(uint2 a, uint2 b) => new uint2(a.x & b.x, a.y & b.y);
+		public static uint2 operator &(uint2 a, uint  b) => new uint2(a.x & b  , a.y & b  );
+		public static uint2 operator &(uint  a, uint2 b) => new uint2(a   & b.x, a   & b.y);
+
+		public static uint2 operator |(uint2 a, uint2 b) => new uint2(a.x | b.x, a.y | b.y);
+		public static uint2 operator |(uint2 a, uint  b) => new uint2(a.x | b  , a.y | b  );
+		public static uint2 operator |(uint  a, uint2 b) => new uint2(a   | b.x, a   | b.y);
+
+		public static uint2 operator ^(uint2 a, uint2 b) => new uint2(a.x ^ b.x, a.y ^ b.y);
+		public static uint2 operator ^(uint2 a, uint  b) => new uint2(a.x ^ b  , a.y ^ b  );
+		public static uint2 operator ^(uint  a, uint2 b) => new uint2(a   ^ b.x, a   ^ b.y);
 	}
 
 	public static partial class math
--- uint3.cs
+++ uint3.cs
@@ -33,6 +33,18 @@ namespace RodMath
 		public static uint3 operator /(uint3 a, uint3 b) => new uint3(a.x / b.x, a.y / b.y, a.z / b.z);
 		public static uint3 operator /(uint3 a, uint  b) => new uint3(a.x / b  , a.y / b  , a.z / b  );
 		public static uint3 operator /(uint  a, uint3 b) => new uint3(a   / b.x, a   / b.y, a   / b.z);
+
+		public static uint3 operator &(uint3 a, uint3 b) => new uint3(a.x & b.x, a.y & b.y, a.z & b.z);
+		public static uint3 operator &(uint3 a, uint  b) => new uint3(a.x & b  , a.y & b  , a.z & b  );
+		public static uint3 operator &(uint  a, uint3 b) => new uint3(a   & b.x, a   & b.y, a   & b.z);
+
+		public static uint3 operator |(uint3 a, uint3 b) => new uint3(a.x | b.x, a.y | b.y, a.z | b.z);
+		public static uint3 operator |(uint3 a, uint  b) => new uint3(a.x | b  , a.y | b  , a.z | b  );
+		public static uint3 operator |(uint  a, uint3 b) => new uint3(a   | b.x, a   | b.y, a   | b.z);
+
+		public static uint3 operator ^(uint3 a, uint3 b) => new uint3(a.x ^ b.x, a.y ^ b.y, a.z ^ b.z);
+		public static uint3 operator ^(uint3 a, uint  b) => new uint3(a.x ^ b  , a.y ^ b  , a.z ^ b  );
+		public static uint3 operator ^(uint  a, uint3 b) => new uint3(a   ^ b.x, a   ^ b.y, a   ^ b.z);
 	}
 
 	public static partial class math
