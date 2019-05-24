0ed9b61b6d0f41c48552e0f654f5e8c097ecb00a
Rodol Phito <rodolvertice@gmail.com>
2019-05-24 02:49:25 -0700

    Added abs for float

--- math.cs
+++ math.cs
@@ -14,5 +14,10 @@ namespace RodMath
 		static bool any(bool2 a) => a.x | a.y;
 		static bool any(bool3 a) => a.x | a.y | a.z;
 		static bool any(bool4 a) => a.x | a.y | a.z | a.w;
+
+		static unsafe float  abs(float  a) { uint  b = (*(uint *) &a) & 0x7FFFFFFF; return *(float *) &b; }
+		static unsafe float2 abs(float2 a) { uint2 b = (*(uint2*) &a) & 0x7FFFFFFF; return *(float2*) &b; }
+		static unsafe float3 abs(float3 a) { uint3 b = (*(uint3*) &a) & 0x7FFFFFFF; return *(float3*) &b; }
+		static unsafe float4 abs(float4 a) { uint4 b = (*(uint4*) &a) & 0x7FFFFFFF; return *(float4*) &b; }
 	}
 }
