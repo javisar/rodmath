6a091be09407a1f1230f4bcebad313a1d2d59399
Rodol Phito <rodolvertice@gmail.com>
2019-05-24 14:45:51 -0700

    Make formatting consistent

--- float4.cs
+++ float4.cs
@@ -20,29 +20,29 @@ namespace RodMath
 			this.w = x;
 		}
 
-		public static bool4 operator ==(float4 a, float4 b) => new bool4 (a.x == b.x, a.y == b.y, a.z == b.z, a.w == b.w);
-		public static bool4 operator ==(float4 a, float  b) => new bool4 (a.x == b  , a.y == b  , a.z == b  , a.w == b  );
-		public static bool4 operator ==(float  a, float4 b) => new bool4 (a   == b.x, a   == b.y, a   == b.z, a   == b.w);
+		public static bool4 operator ==(float4 a, float4 b) => new bool4(a.x == b.x, a.y == b.y, a.z == b.z, a.w == b.w);
+		public static bool4 operator ==(float4 a, float  b) => new bool4(a.x == b  , a.y == b  , a.z == b  , a.w == b  );
+		public static bool4 operator ==(float  a, float4 b) => new bool4(a   == b.x, a   == b.y, a   == b.z, a   == b.w);
 
-		public static bool4 operator !=(float4 a, float4 b) => new bool4 (a.x != b.x, a.y != b.y, a.z != b.z, a.w != b.w);
-		public static bool4 operator !=(float4 a, float  b) => new bool4 (a.x != b  , a.y != b  , a.z != b  , a.w != b  );
-		public static bool4 operator !=(float  a, float4 b) => new bool4 (a   != b.x, a   != b.y, a   != b.z, a   != b.w);
+		public static bool4 operator !=(float4 a, float4 b) => new bool4(a.x != b.x, a.y != b.y, a.z != b.z, a.w != b.w);
+		public static bool4 operator !=(float4 a, float  b) => new bool4(a.x != b  , a.y != b  , a.z != b  , a.w != b  );
+		public static bool4 operator !=(float  a, float4 b) => new bool4(a   != b.x, a   != b.y, a   != b.z, a   != b.w);
 
-		public static bool4 operator < (float4 a, float4 b) => new bool4 (a.x <  b.x, a.y <  b.y, a.z <  b.z, a.w <  b.w);
-		public static bool4 operator < (float4 a, float  b) => new bool4 (a.x <  b  , a.y <  b  , a.z <  b  , a.w <  b  );
-		public static bool4 operator < (float  a, float4 b) => new bool4 (a   <  b.x, a   <  b.y, a   <  b.z, a   <  b.w);
+		public static bool4 operator < (float4 a, float4 b) => new bool4(a.x <  b.x, a.y <  b.y, a.z <  b.z, a.w <  b.w);
+		public static bool4 operator < (float4 a, float  b) => new bool4(a.x <  b  , a.y <  b  , a.z <  b  , a.w <  b  );
+		public static bool4 operator < (float  a, float4 b) => new bool4(a   <  b.x, a   <  b.y, a   <  b.z, a   <  b.w);
 
-		public static bool4 operator > (float4 a, float4 b) => new bool4 (a.x >  b.x, a.y >  b.y, a.z >  b.z, a.w >  b.w);
-		public static bool4 operator > (float4 a, float  b) => new bool4 (a.x >  b  , a.y >  b  , a.z >  b  , a.w >  b  );
-		public static bool4 operator > (float  a, float4 b) => new bool4 (a   >  b.x, a   >  b.y, a   >  b.z, a   >  b.w);
+		public static bool4 operator > (float4 a, float4 b) => new bool4(a.x >  b.x, a.y >  b.y, a.z >  b.z, a.w >  b.w);
+		public static bool4 operator > (float4 a, float  b) => new bool4(a.x >  b  , a.y >  b  , a.z >  b  , a.w >  b  );
+		public static bool4 operator > (float  a, float4 b) => new bool4(a   >  b.x, a   >  b.y, a   >  b.z, a   >  b.w);
 
-		public static bool4 operator <=(float4 a, float4 b) => new bool4 (a.x <= b.x, a.y <= b.y, a.z <= b.z, a.w <= b.w);
-		public static bool4 operator <=(float4 a, float  b) => new bool4 (a.x <= b  , a.y <= b  , a.z <= b  , a.w <= b  );
-		public static bool4 operator <=(float  a, float4 b) => new bool4 (a   <= b.x, a   <= b.y, a   <= b.z, a   <= b.w);
+		public static bool4 operator <=(float4 a, float4 b) => new bool4(a.x <= b.x, a.y <= b.y, a.z <= b.z, a.w <= b.w);
+		public static bool4 operator <=(float4 a, float  b) => new bool4(a.x <= b  , a.y <= b  , a.z <= b  , a.w <= b  );
+		public static bool4 operator <=(float  a, float4 b) => new bool4(a   <= b.x, a   <= b.y, a   <= b.z, a   <= b.w);
 
-		public static bool4 operator >=(float4 a, float4 b) => new bool4 (a.x >= b.x, a.y >= b.y, a.z >= b.z, a.w >= b.w);
-		public static bool4 operator >=(float4 a, float  b) => new bool4 (a.x >= b  , a.y >= b  , a.z >= b  , a.w >= b  );
-		public static bool4 operator >=(float  a, float4 b) => new bool4 (a   >= b.x, a   >= b.y, a   >= b.z, a   >= b.w);
+		public static bool4 operator >=(float4 a, float4 b) => new bool4(a.x >= b.x, a.y >= b.y, a.z >= b.z, a.w >= b.w);
+		public static bool4 operator >=(float4 a, float  b) => new bool4(a.x >= b  , a.y >= b  , a.z >= b  , a.w >= b  );
+		public static bool4 operator >=(float  a, float4 b) => new bool4(a   >= b.x, a   >= b.y, a   >= b.z, a   >= b.w);
 
 		public static float4 operator +(float4 a, float4 b) => new float4(a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w);
 		public static float4 operator +(float4 a, float  b) => new float4(a.x + b  , a.y + b  , a.z + b  , a.w + b  );
